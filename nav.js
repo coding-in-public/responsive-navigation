4ac56619bdb302520475504ab1423f22937c7b5d
Coding in Public <86967271+coding-in-public@users.noreply.github.com>
2022-03-14 11:55:22 -0600

    Finalized Code

---
+++ nav.js
@@ -0,0 +1,33 @@
+const navBtn = document.querySelector('#menu-btn');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelector('.nav-links');
+
+navBtn.addEventListener('click', () => {
+  const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
+  navBtn.setAttribute('aria-expanded', !isExpanded);
+  !isExpanded && nav.classList.add('active');
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  navLinks.classList.add('activated');
+})
+
+// INTERSECTION OBSERVERS
+const navObserver = new IntersectionObserver((watchEntry) => {
+  !watchEntry[0].isIntersecting ? nav.classList.add('active') : nav.classList.remove('active');
+}, {threshold: 0.85});
+
+navObserver.observe(document.querySelector('header'));
+
+const fadeUpObserver = new IntersectionObserver((elsToWatch) => {
+  elsToWatch.forEach((el) => {
+    if (el.isIntersecting) {
+      el.target.classList.add('faded');
+      fadeUpObserver.unobserve(el.target);
+    }
+  });
+}, {threshold: 0.25});
+
+document.querySelectorAll('.fade-up').forEach((item) => {
+  fadeUpObserver.observe(item);
+});
